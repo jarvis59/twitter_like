97da47254f022530237c4754d20b3d5b2b7ca479
MS <msulek59@hotmail.com>
2020-08-09 15:13:44 +0200

    database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse # useless if HttpResponse is off
 
 # def home(request):
@@ -7,23 +8,24 @@ from django.shortcuts import render
 # def about(request):
 #     return HttpResponse('<h1>Blog About</h1>')
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 29, 2018'
-    },
-]
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 29, 2018'
+#     },
+# ]
+
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
