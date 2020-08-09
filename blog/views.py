0a7f89b148b0bbc98a277138d37ad1f5df323c0e
MS <msulek59@hotmail.com>
2020-08-09 08:50:41 +0200

    added templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+# from django.http import HttpResponse # useless if HttpResponse is off
 
-# Create your views here.
+# def home(request):
+#     return HttpResponse('<h1>Blog Home</h1>')
+
+# def about(request):
+#     return HttpResponse('<h1>Blog About</h1>')
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 29, 2018'
+    },
+]
+def home(request):
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
